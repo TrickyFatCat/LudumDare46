c394e3215367cba69cbf6479a79bf8e4e6c974ee
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-04-19 20:49:54 +0300

    Added background with texture moving shader

--- src/environment/BonusFruit.gd
+++ src/environment/BonusFruit.gd
@@ -3,6 +3,7 @@ extends Area2D
 export(int) var points_cost = 1
 
 
+# warning-ignore:unused_argument
 func _on_BonusFruit_body_entered(body):
 	PointsHandler.increase_points(points_cost)
 	queue_free()
